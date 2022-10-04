0792448781a81a2f1644cbe1bdc8ec910c601140
Heorhi Puhachou <h.puhachou@gmail.com>
2022-10-04 08:53:22 +0200

    Directory checking (I would like to find layer code).

--- start-point/lambda/statemachine/step3/step3.js
+++ start-point/lambda/statemachine/step3/step3.js
@@ -5,17 +5,35 @@ exports.handler = (event, context, callback) => {
     console.log("Step 3 start");
     const fs = require('fs')
 
-// directory to check if exists
+    // directory to check if exists
     const dir = './opt'
 
-// check if directory exists
     if (fs.existsSync(dir)) {
         console.log('Directory exists!')
     } else {
         console.log('Directory not found.')
     }
 
+    checkFolder('./opt');
+    checkFolder('./opt/code/test');
+    checkFolder('opt');
+    checkFolder('code');
+    checkFolder('nodejs');
 
     //run(event, context, callback);
     console.log("Step 3 end");
+
+    event.from = "Step 3";
+    callback(null, event)
 };
+
+// check if directory exists
+let checkFolder = (name) => {
+    const fs = require('fs')
+
+    if (fs.existsSync(name)) {
+        console.log(name + ' directory exists!')
+    } else {
+        console.log(name + ' directory not found.')
+    }
+}
